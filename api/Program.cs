96d3c2fa6d85ec7bbe68cee10620de478bb34060
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-04-13 18:55:49 -0300

    created api with solution

---
+++ api/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+// A single endpoint that returns a greeting message.
+app.MapGet("/", () => "Hello from .NET API!")
+.WithName("home")
+.WithOpenApi();
+
+app.Run();
