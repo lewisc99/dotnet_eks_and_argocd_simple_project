e80171d645a42603f3825a9852e639af05f7bc2e
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-04-14 21:00:09 -0300

    fix: update Terraform configuration for AWS provider and EKS cluster, remove unused output

--- my-eks-argo-setup/terraform/main.tf
+++ my-eks-argo-setup/terraform/main.tf
@@ -2,15 +2,24 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Create a new VPC (or reference an existing one)
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.75"
+    }
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.2"
+  version = "3.19.0"
 
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  name            = "eks-vpc"
+  cidr            = "10.0.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
@@ -18,19 +27,16 @@ module "vpc" {
   single_nat_gateway = true
 }
 
-data "aws_availability_zones" "available" {}
-
-# Create EKS Cluster
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "18.0.0"
-  cluster_name    = var.cluster_name
-  cluster_version = "1.21"
+  source       = "terraform-aws-modules/eks/aws"
+  version      = "18.0.0"
+  cluster_name = var.cluster_name
+  # Change the version to a supported value (here set to "1.24")
+  cluster_version = "1.30"
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
 }
 
-# Configure local Kubernetes provider using the EKS cluster data
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
@@ -45,7 +51,6 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
 
-# Install Argo CD using the Helm provider
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
@@ -60,10 +65,12 @@ resource "helm_release" "argocd" {
   create_namespace = true
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "5.29.2" # Use the version you prefer
+  version          = "7.8.24"
 
   set {
     name  = "server.service.type"
     value = "LoadBalancer"
   }
+
+  timeout = 600
 }
